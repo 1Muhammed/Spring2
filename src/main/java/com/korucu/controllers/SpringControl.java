8b1532828956a42d043d6c6f8931f947ad40ac59
vektorel <1muhammed.korucu@gmail.com>
2018-08-04 16:52:15 +0300

    12. yayın

--- src/main/java/com/korucu/controllers/SpringControl.java
+++ src/main/java/com/korucu/controllers/SpringControl.java
@@ -1,10 +1,11 @@
 package com.korucu.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-
+@RestController
+@EnableAutoConfiguration
 public class SpringControl {
 
 	@RequestMapping("/spring")
